a6b991942c98319b4c1db15dbdd0a7ac3beb7b47
Hem Pun <hemthegentlmen@gmail.com>
2023-05-21 08:40:11 +0545

    Asses for website added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { RiMenu3Line, RiCloseLin, RiLogoutBoxFill } from "react-icons/ri";
+import "./Navbar.css";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3_navbar">
+      <div className="gpt3_navbar-links">
+        <div className="gpt3_navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
