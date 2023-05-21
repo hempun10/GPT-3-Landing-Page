26ea461ef0467e4bfc003d9aae7dc2676120ca62
Hem Pun <hemthegentlmen@gmail.com>
2023-05-21 12:35:35 +0545

    Done with Features and CTA Section

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import "./features.css";
+import { Feature } from "../../components";
+const FeaturesData = [
+  {
+    title: "Improving end distrusts instantly ",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded. ",
+  },
+  {
+    title: "Become the tended active ",
+    text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to. ",
+  },
+  {
+    title: "Message or am nothing",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address. ",
+  },
+  {
+    title: "Really boy law county ",
+    text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush. ",
+  },
+];
 
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className="gpt3_features section_padding">
+      <div className="gpt3_features_heading">
+        <h1 className="gradient_text">
+          The Future is Now and You Just Need to Realize It. Step into Future
+          Today & Make it Happen
+        </h1>
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3_features-container">
+        {FeaturesData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={index} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Features;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,7 +1,43 @@
-import React from "react";
-
+import "./whatGPT3.css";
+import { Feature } from "../../components";
 const WhatGPT3 = () => {
-  return <div>what</div>;
+  return (
+    <div className="gpt3_whatgpt3 section_margin" id="wgpt3">
+      <div className="gpt3_whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="Starting with the very basics, GPT-3 stands for Generative Pre-trained Transformer 3 – it's the third version of the tool to be released. In short, this means that it generates text using algorithms that are pre-trained – they've already been fed all of the data they need to carry out their task.
+"
+        />
+      </div>
+      <div className="gpt3_whatgpt3-heading">
+        <h1 className="gradient_text">
+          The possibility beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3_whatgpt3-container">
+        <Feature
+          title={"Chat Bots"}
+          text={
+            "Enhancing automated instant messaging services with GPT-3 technology provides the tools to analyze and understand customer queries and carry on a human-like conversation. GPT-3 adds an instant boost of contextual understanding and pattern recognition to scripted bots."
+          }
+        />
+        <Feature
+          title={"Knowledge base"}
+          text={
+            "That model can then be used to generate prose (or even code) that seems like it was written by a real person. In simple terms, language models help computers estimate the probability of word sequences."
+          }
+        />
+        <Feature
+          title={"Eduction"}
+          text={
+            "GPT-3 can be used to help students write better essays, reports, speeches and stories. The AI-based tool is capable of analyzing more than a hundred thousand sentences per second to ensure that the content being produced is grammatically correct."
+          }
+        />
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
